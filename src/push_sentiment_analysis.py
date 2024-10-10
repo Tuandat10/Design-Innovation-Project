042a7fa5eeac97ebd4b667ef1f9fe4cee4a4fdcf
Tuan Dat Nguyen <shironeko10112000@gmail.com>
2024-10-11 00:12:08 +1100

    finish add key_word

--- src/push_sentiment_analysis.py
+++ src/push_sentiment_analysis.py
@@ -1,51 +1,93 @@
 import mysql.connector
 import pandas as pd
 import os
+from openai import OpenAI
+import re
 host = "feenix-mariadb.swin.edu.au"
 user = "s104489467"
 password = "101100"
 database = "s104489467_db"
-def push_data(path):
-    try:
-        connection = mysql.connector.connect(host=host,user=user,password=password,database=database)
-        cursor = connection.cursor()
-    except:
-        print("Error in connection")
-    if path == r"C:\Users\HP Envy\OneDrive - Swinburne University\Data Science\Innovation project\craw_data\Dat":
-        path = path + "\\" + "output_sentiment.csv"
-        df = pd.read_csv(path)
-        for i in range(len(df)):
-            country = df['country'][i]
-            energy_source = df['type'][i]
-            comment = df["comment"][i]
-            sentiment_score = df['rescaled_compound'][i]
-            sql_query = "INSERT INTO public_sentiment (country, energy_source, comment, sentiment_score) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql_query,(country,energy_source,comment,sentiment_score))
-            connection.commit()
-    else:
-        directory = path
-        path_names = os.listdir(directory)
-        australia = "Australia"
-        china = "China"
-        japan = "Japan"
-        country_list = [australia,china,japan]
-        for path in path_names:
-            path = directory + "\\" + path
-            for i in country_list:
-                if i in path:
-                    country = i
-                    break
-            df = pd.read_csv(path)
-            for i in range(len(df)):
-                energy_source = df['Category'][i]
-                comment = df["Post Body"][i]
-                sentiment_score = df['bert_sentiment'][i]
-                sql_query = "INSERT INTO public_sentiment (country, energy_source, comment, sentiment_score) VALUES (%s, %s, %s, %s)"
-                cursor.execute(sql_query,(country,energy_source,comment,sentiment_score))
-                connection.commit()
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+def push_data():
+    paths = [r'C:\Users\HP Envy\OneDrive - Swinburne University\Data Science\Innovation project\craw_data\Dat',r'C:\Users\HP Envy\OneDrive - Swinburne University\Data Science\Innovation project\craw_data\Jun\data']
+    df= pd.DataFrame(columns=['country','energy_source','comment','sentiment_score'])
+    for path in paths:
+        # try:
+        #     print("go try")
+        #     connection = mysql.connector.connect(host=host,user=user,password=password,database=database)
+        #     cursor = connection.cursor()
+        # except:
+        #     print("Error in connection")
+        if path == r"C:\Users\HP Envy\OneDrive - Swinburne University\Data Science\Innovation project\craw_data\Dat":
+            print("go if")
+            path = path + "\\" + "output_sentiment.csv"
+            df2 = pd.read_csv(path)
+            for i in range(len(df2)):
+                country = df2['country'][i]
+                energy_source = df2['type'][i]
+                comment = df2["comment"][i]
+                sentiment_score = df2['rescaled_compound'][i]
+                df1 = pd.DataFrame({'country':[country],'energy_source':[energy_source],'comment':[comment],'sentiment_score':[sentiment_score]})
+                df = pd.concat([df,df1],ignore_index=True)
+                # sql_query = "INSERT INTO public_sentiment (country, energy_source, comment, sentiment_score) VALUES (%s, %s, %s, %s)"
+                # cursor.execute(sql_query,(country,energy_source,comment,sentiment_score))
+                # connection.commit()
+        else:
+            directory = path
+            path_names = os.listdir(directory)
+            australia = "Australia"
+            china = "China"
+            japan = "Japan"
+            country_list = [australia,china,japan]
+            for path in path_names:
+                path = directory + "\\" + path
+                for i in country_list:
+                    if i in path:
+                        country = i
+                        break
+                df2 = pd.read_csv(path)
+                for i in range(len(df2)):
+                    energy_source = df2['Category'][i]
+                    comment = df2["Post Body"][i]
+                    sentiment_score = float(df2['bert_sentiment'][i])
+                    df1 = pd.DataFrame({'country':[country],'energy_source':[energy_source],'comment':[comment],'sentiment_score':[sentiment_score]})
+                    df = pd.concat([df,df1],ignore_index=True)
+                    # sql_query = "INSERT INTO public_sentiment (country, energy_source, comment, sentiment_score) VALUES (%s, %s, %s, %s)"
+                    # cursor.execute(sql_query,(country,energy_source,comment,sentiment_score))
+                    # connection.commit()
+        # cursor.close()
+        # connection.close()
+        print("End Push a File")
+    return df
+
+# path = input("Enter the path of the file: ")
+def add_keywords(keyword):
+    completion = client.chat.completions.create(
+    model="TheBloke/Mistral-7B-OpenOrca-GGUF",
+    messages=[
+      {"role": "system", "content": "Assign these types of keyword for each comment in these scope: Policy, Jobs, Economy, Tax, Investment, Technology, Environment, Cost, Infrastructure. One comment can have more than one keywords just give me keywords  and do not explain"},
+      {"role": "user", "content": keyword}
+    ],
+    temperature=0.7,
+  )
+    pattern = r"content\s*=\s*'([^']*)'"
+    keyword = re.findall(pattern, str(completion))[0]
+    return keyword
+def main():
+    df = push_data()
+    connection = mysql.connector.connect(host=host,user=user,password=password,database=database)
+    cursor = connection.cursor()
+    for i in range(len(df)):
+        country = df['country'][i]
+        energy_source = df['energy_source'][i]
+        comment = df['comment'][i]
+        keyword = add_keywords(comment)
+        sentiment_score = df['sentiment_score'][i]
+        sql_query = "INSERT INTO public_sentiment (country, energy_source, comment, key_word, sentiment_score) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql_query,(country,energy_source,comment,keyword,sentiment_score))
+        connection.commit()
     cursor.close()
-    connection.close()
-    print("End Push a File")
-path = input("Enter the path of the file: ")
-push_data(path)
+    connection.close()      
+main()
     
